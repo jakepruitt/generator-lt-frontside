4ebb1efd804fd602f8c6c42ce95ae5bb27ed5901
Jake Pruitt <jrpruit1@asu.edu>
2015-01-22 15:10:14 -0700

    Working on prepping for jade

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ var path = require('path');
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
+		this.sourceRoot(path.join(__dirname, '../templates/common'));
   },
 
   prompting: function () {
@@ -19,10 +20,18 @@ module.exports = yeoman.generators.Base.extend({
       name: 'projectName',
       message: 'What would you like to name your project?',
       default: path.basename(process.cwd())
-    }];
+    }, {
+			type: 'confirm',
+			name: 'jade',
+			message: 'Would you like to use Jade?',
+			default: false
+		}];
 
     this.prompt(prompts, function (props) {
       this.projectName = props.projectName;
+			this.projectname = this._.camelize(this._.slugify(this._.humanize(this.projectName)));
+			
+			this.jade = props.jade;
 
       done();
     }.bind(this));
@@ -33,6 +42,7 @@ module.exports = yeoman.generators.Base.extend({
 			this.template('_bower.json', 'bower.json');
 			this.template('_package.json', 'package.json');
 			this.template('_README.md', 'README.md');
+			this.template('_Gruntfile.js', 'Gruntfile.js');
     },
 
     projectfiles: function () {
@@ -53,11 +63,30 @@ module.exports = yeoman.generators.Base.extend({
         this.destinationPath('.gitignore')
       );
       this.fs.copy(
-        this.templatePath('Gruntfile.js'),
-        this.destinationPath('Gruntfile.js')
+        this.templatePath('src/robots.txt'),
+        this.destinationPath('src/robots.txt')
       );
-			this.directory('src');
-    }
+			this.directory('src/js');
+			this.directory('src/scss');
+			this.directory('src/images');
+    },
+
+		jade: function() {
+			if(this.jade) {
+				this.sourceRoot(path.join(__dirname, '../templates/jade'));
+				this.fs.copy(
+					this.templatePath('src/index.jade'),
+					this.destinationPath('src/index.jade')
+				);
+				this.directory('src/layout');
+				this.directory('src/partials');
+			} else {
+				this.fs.copy(
+					this.templatePath('src/index.html'),
+					this.destinationPath('src/index.html')
+				);
+			}
+		}
   },
 
   install: function () {
